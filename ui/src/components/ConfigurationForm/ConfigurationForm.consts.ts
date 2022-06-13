2ffaab4016116cb12c03d6013561eaa055cafc41
Robert Lubarski <24817919+lubarskyy@users.noreply.github.com>
2022-06-13 18:52:45 +0200

    imp(): adjust group id field validation to match backend regexp, make effect implementation validatin based on effect type selected

--- ui/src/components/ConfigurationForm/ConfigurationForm.consts.ts
+++ ui/src/components/ConfigurationForm/ConfigurationForm.consts.ts
@@ -2,6 +2,11 @@ import * as yup from 'yup';
 import { EffectType, EffectImplementation, JSONImplementation } from 'api/starter';
 import type { FormSelectOption } from '../FormSelect';
 import type { FormRadioOption } from '../FormRadioGroup';
+import {
+  mapEffectTypeToEffectImplementation,
+  mapEffectTypeToJSONImplementation,
+  getEffectImplementationOptions,
+} from './ConfigurationForm.helpers';
 
 export const TAPIR_VERSION_OPTIONS: FormSelectOption<string>[] = [
   {
@@ -74,36 +79,84 @@ export const JSON_OUTPUT_OPTIONS: FormRadioOption<JSONImplementation>[] = [
   },
 ];
 
+const labelGetter = (
+  option: FormSelectOption | FormRadioOption
+): FormSelectOption['label'] | FormRadioOption['label'] => option.label;
+
+const valueGetter = (
+  option: FormSelectOption | FormRadioOption
+): FormSelectOption['value'] | FormRadioOption['value'] => option.value;
+
+const getEffectImplementationFieldMessage = (effectType: EffectType): string =>
+  `Server implementation must be one of the following values: ${getEffectImplementationOptions(effectType)
+    .map(labelGetter)
+    .join(', ')}`;
+
+const REQUIRED_FIELD_MESSAGE = 'This field is required';
+
 export const starterValidationSchema = yup
   .object({
     projectName: yup
       .string()
       .strict()
       .matches(/^[a-z0-9]+$/, 'Project name can consists of only lowercase characters and numbers')
-      .required('This field is required'),
+      .required(REQUIRED_FIELD_MESSAGE),
     groupId: yup
       .string()
       .strict()
-      .lowercase('GroupId should follow Java package naming convention')
-      .max(256, 'GroupId should be smaller than 256 characters')
-      .required('This field is required'),
-    tapirVersion: yup.string().required('This field is required'),
+      .matches(
+        /(?:^[a-z][a-z0-9_]*|[a-z][a-z0-9_]*\.[a-z0-9_]+)+$/,
+        'Group ID should follow Java package naming convention'
+      )
+      .max(256, 'Group ID length should be smaller than 256 characters')
+      .required(REQUIRED_FIELD_MESSAGE),
+    tapirVersion: yup.string().required(REQUIRED_FIELD_MESSAGE),
     effect: yup
       .mixed()
       .oneOf(
-        Object.values(EffectType),
-        `Effect type must be one of the following values: ${Object.keys(EffectType).join(', ')}`
+        EFFECT_TYPE_OPTIONS.map(valueGetter),
+        `Effect type must be one of the following values: ${EFFECT_TYPE_OPTIONS.map(labelGetter).join(', ')}`
       )
-      .required('This field is required'),
+      .required(REQUIRED_FIELD_MESSAGE),
+    // NOTE: unfortunately this is the only way of multiple .when cases in yup :shrug-emoji:
     implementation: yup
       .mixed()
-      .oneOf(
-        Object.values(EffectImplementation),
-        /* eslint-disable no-template-curly-in-string */
-        'Server implementation must be one of the following values: ${values}'
-      )
-      .required('This field is required'),
-    addDocumentation: yup.boolean().required('This field is required'),
-    json: yup.mixed().oneOf(Object.values(JSONImplementation)).required('This field is required'),
+      .when('effect', {
+        is: EffectType.Future,
+        then: schema =>
+          schema.oneOf(
+            mapEffectTypeToEffectImplementation(EffectType.Future),
+            getEffectImplementationFieldMessage(EffectType.Future)
+          ),
+        otherwise: schema =>
+          schema.when('effect', {
+            is: EffectType.IO,
+            then: schema =>
+              schema.oneOf(
+                mapEffectTypeToEffectImplementation(EffectType.IO),
+                getEffectImplementationFieldMessage(EffectType.IO)
+              ),
+            otherwise: schema =>
+              schema.when('effect', {
+                is: EffectType.ZIO,
+                then: schema =>
+                  schema.oneOf(
+                    mapEffectTypeToEffectImplementation(EffectType.ZIO),
+                    getEffectImplementationFieldMessage(EffectType.ZIO)
+                  ),
+              }),
+          }),
+      })
+      .required(REQUIRED_FIELD_MESSAGE),
+    addDocumentation: yup.boolean().required(REQUIRED_FIELD_MESSAGE),
+    json: yup
+      .mixed()
+      .when('effect', {
+        is: (effect: EffectType) => effect !== EffectType.ZIO,
+        // NOTE: any effect type besides ZIO will work here as an argument
+        then: schema => schema.oneOf(mapEffectTypeToJSONImplementation(EffectType.Future)),
+        otherwise: schema => schema.oneOf(mapEffectTypeToJSONImplementation(EffectType.ZIO)),
+      })
+      .required(REQUIRED_FIELD_MESSAGE),
   })
   .required();
