876a8dfcfba0c5d621de8f85e02ec47c7c748b86
Jacek Centkowski <jcentkowski@softwaremill.com>
2022-07-19 11:07:50 +0200

    feat: test if hello endpoint works in a generated configuration
    Extend existing IT test so that additionally 'sbt run' (to obtain
    port) is called in a preparation phase of the given integration test.
    
    The 'hello' endpoint tests are always performed but docs and metrics
    are called conditionally (at present they are empty) if they are
    part of a specific configuration.
    
    Here is the output example:
    [info] StarterServiceITTest:
    [info] - should return zip file containing working sbt folder with: FutureEffect/Akka/docs=false/metrics=true/WithoutJson
    [info] - should return zip file containing working sbt folder with: FutureEffect/Akka/docs=true/metrics=true/WithoutJson
    
    Note that in case when any part of preparation phase (compile, test or
    run) its std out (err is merged to out) will be printed in logs.
    In addition there is a safety 60s timeout employed for waiting for
    a process to start and additional 60s for all tests for a given
    configuration to finish.
    
    Related to #13

--- backend/src/test/scala/com/softwaremill/adopttapir/starter/StarterServiceITTest.scala
+++ backend/src/test/scala/com/softwaremill/adopttapir/starter/StarterServiceITTest.scala
@@ -1,18 +1,27 @@
 package com.softwaremill.adopttapir.starter
 
 import better.files.{FileExtensions, File => BFile}
-import cats.effect.IO
+import cats.effect.{IO, Resource}
 import cats.effect.unsafe.implicits.global
+import cats.instances.list._
+import cats.syntax.parallel._
 import com.softwaremill.adopttapir.starter.api._
 import com.softwaremill.adopttapir.template.ProjectTemplateInTests
 import com.softwaremill.adopttapir.test.BaseTest
-import org.scalatest.ParallelTestExecution
+import org.scalatest.{Assertion, Assertions, ParallelTestExecution}
+import os.SubProcess
+import sttp.client3.{HttpURLConnectionBackend, Identity, SttpBackend, UriContext, asStringAlways, basicRequest}
 
-import java.io.File
+import java.io.{PrintWriter, StringWriter}
+import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.concurrent.TimeoutException
+import scala.concurrent.duration.DurationInt
+import scala.util.Using
+import scala.util.matching.Regex
 
-class StarterServiceITTest extends BaseTest with ParallelTestExecution {
-
-  for {
+object Setup {
+  val validConfigurations: Seq[StarterDetails] = for {
     effect <- EffectRequest.values
     server <- ServerImplementationRequest.values
     docs <- List(true, false)
@@ -20,32 +29,180 @@ class StarterServiceITTest extends BaseTest with ParallelTestExecution {
     json <- JsonImplementationRequest.values
     starterRequest = StarterRequest("myproject", "com.softwaremill", effect, server, addDocumentation = docs, addMetrics = metrics, json)
     starterDetails <- FormValidator.validate(starterRequest).toSeq
-  } {
-    it should s"return zip file containing working sbt folder with: $effect/$server/docs=$docs/metrics=$metrics/$json" in {
-      val service = createStarterService
-      sbtCompileTest(service.generateZipFile(starterDetails))
+  } yield starterDetails
+
+  implicit class StarterDetailsWithDescribe(details: StarterDetails) {
+    lazy val describe: String =
+      s"${details.serverEffect}/${details.serverImplementation}/docs=${details.addDocumentation}/metrics=${details.addMetrics}/${details.jsonImplementation}"
+  }
+
+  type TestFunction = Integer => IO[Assertion]
+}
+
+class StarterServiceITTest extends BaseTest with ParallelTestExecution {
+  import Setup._
+
+  for { details <- Setup.validConfigurations } {
+    it should s"return zip file containing working sbt folder with: ${details.describe}" in {
+      val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()
+
+      // define endpoints integration tests
+      val helloEndpointTest: Option[TestFunction] = Some(port =>
+        IO.blocking {
+          val result: String =
+            basicRequest.response(asStringAlways).get(uri"http://localhost:$port/hello?name=Frodo").send(backend).body
+
+          result shouldBe "Hello Frodo"
+        }
+      )
+
+      val docsEndpointTest: Option[TestFunction] = Option.when(details.addDocumentation)(_ =>
+        IO.blocking {
+          assert(true)
+          // TODO write some tests
+        }
+      )
+
+      val metricsEndpointTest: Option[TestFunction] = Option.when(details.addMetrics)(_ =>
+        IO.blocking {
+          assert(true)
+          // TODO write some tests
+        }
+      )
+
+      // get implementation for a given configuration, compile it, unit & integration test it
+      val service = ServiceUnderTest(details)
+      service.run(List(helloEndpointTest, docsEndpointTest, metricsEndpointTest).flatten)
+    }
+  }
+}
+
+case class ServiceUnderTest(details: StarterDetails) {
+  import Setup._
+
+  def run(tests: List[TestFunction]): Unit = {
+    val log = new mutable.StringBuilder()
+    (for {
+      zipFile <- generateZipFile(details, log)
+      tempDir <- createTempDirectory()
+    } yield (zipFile, tempDir))
+      .use { case (zipFile, tempDir) =>
+        unzipFile(zipFile, tempDir, log) >> spawnService(tempDir).use(service =>
+          getPortFromService(service, log).flatMap(port => runTests(port, tests, log))
+        )
+      }
+      .unsafeRunSync()
+  }
+
+  private def generateZipFile(details: StarterDetails, log: mutable.StringBuilder): Resource[IO, BFile] = {
+    Resource.make(for {
+      zipFile <- ZipGenerator.service.generateZipFile(details).map(_.toScala)
+      _ <- logStep(log, "* zip file was generated")
+    } yield zipFile)(zipFile => IO.blocking(zipFile.delete()))
+  }
+
+  private def createTempDirectory(): Resource[IO, BFile] = {
+    Resource.make(IO.blocking(BFile.newTemporaryDirectory("sbtTesting")))(tempDir => IO.blocking(tempDir.delete()))
+  }
+
+  private def unzipFile(zipFile: BFile, tempDir: BFile, log: mutable.StringBuilder): IO[Unit] = {
+    IO.blocking(zipFile.unzipTo(tempDir)) >> logStep(log, "* zip file was unzipped")
+  }
+
+  private def spawnService(tempDir: BFile): Resource[IO, Service] = {
+    Resource.make(IO.blocking(Service(tempDir)))(_.close())
+  }
+
+  private def getPortFromService(service: Service, log: mutable.StringBuilder): IO[Integer] = {
+    for {
+      port <- service.port
+      _ <- logStep(log, s"* service compiled, tested & started on port $port")
+    } yield port
+  }
+
+  private def runTests(port: Integer, tests: List[TestFunction], log: mutable.StringBuilder): IO[Unit] = {
+    tests
+      .map(_(port))
+      .parSequence
+      .timeoutAndForget(30.seconds)
+      .onError(e =>
+        Assertions.fail(
+          s"Only the following test steps were finished for configuration '${details.describe}':$log\n${describe(e)}"
+        )
+      ) >> logStep(
+      log,
+      s"* integration tests on port $port were finished"
+    )
+  }
+
+  private def logStep(steps: mutable.StringBuilder, step: String): IO[Unit] = IO { val _ = steps.append('\n').append(step) }
+
+  private def describe(e: Throwable) = {
+    Using.Manager { _ =>
+      val sw = new StringWriter()
+      val pw = new PrintWriter(sw)
+      e.printStackTrace(pw)
+      sw.toString
+    }.get
+  }
+}
+
+case class Service(tempDir: better.files.File) {
+  private val portPattern = new Regex("^(?:\\[info\\] )(?:Go to |Server started at )http://localhost:(\\d+).*")
+
+  private val process: SubProcess = {
+    os.proc(
+      "sbt",
+      "-no-colors",
+      // start in forked mode so that process input can be forwarded and process waits before closing otherwise `StdIn.readLine` will exit immediately
+      "set run / fork := true",
+      // forward std input to forked process - https://www.scala-sbt.org/1.x/docs/Forking.html#Configuring+Input
+      "set run / connectInput := true",
+      ";compile ;test ;run"
+    ).spawn(cwd = os.Path(tempDir.toJava), env = Map("http.port" -> "0"), mergeErrIntoOut = true)
+  }
+
+  val port: IO[Integer] = {
+    val stdOut = new mutable.StringBuilder()
+    val timeout = 60.seconds
+    IO.blocking {
+      val port = waitForPort(stdOut)
+      assert(port > -1)
+      port
+    }.timeoutAndForget(timeout)
+      .onError(e =>
+        Assertions.fail(
+          s"Detecting port of the running server failed${if (e.isInstanceOf[TimeoutException]) s" due to timeout [${timeout}s]" else ""} with process std output:\n$stdOut"
+        )
+      )
+  }
+
+  @tailrec
+  private def waitForPort(stdOut: mutable.StringBuilder): Integer = {
+    if (process.stdout.available() > 0 || process.isAlive()) {
+      val line = process.stdout.readLine()
+      if (line == null) {
+        -1
+      } else {
+        stdOut.append("### process log <").append(line).append(">").append('\n')
+        portPattern.findFirstMatchIn(line) match {
+          case Some(port) => port.group(1).toInt
+          case None       => waitForPort(stdOut)
+        }
+      }
+    } else {
+      -1
     }
   }
 
-  private def createStarterService = {
+  def close(): IO[Unit] = IO.blocking {
+    if (process.isAlive()) process.close()
+  }
+}
+
+object ZipGenerator {
+  val service: StarterServiceInTests = {
     val config = StarterConfig(deleteTempFolder = true, tempPrefix = "sbtService", scalaVersion = "2.13.8")
     new StarterServiceInTests(config, new ProjectTemplateInTests(config))
-  }
-
-  private def sbtCompileTest(file: IO[File]): Unit = {
-    IO.blocking(BFile.newTemporaryDirectory("sbtTesting"))
-      .bracket { tempDir =>
-        for {
-          zipFile <- file.map(_.toScala)
-          _ <- IO.blocking {
-            zipFile.unzipTo(tempDir)
-            zipFile.delete()
-          }
-          _ <- IO.blocking(os.proc("sbt", ";compile ;test").call(cwd = os.Path(tempDir.toJava)).exitCode shouldBe 0)
-
-        } yield zipFile
-
-      }(tempDir => IO.blocking(tempDir.delete()))
-      .unsafeRunSync()
   }
 }
