a6453aaecfabd53943202654fbb96d4abd2fe400
marcinjozefowicz <marcinjoz87@gmail.com>
2022-08-01 09:34:09 +0200

    Update imports to use asterisk instead of underscore for Scala 3

--- backend/src/main/scala/com/softwaremill/adopttapir/template/scala/Import.scala
+++ backend/src/main/scala/com/softwaremill/adopttapir/template/scala/Import.scala
@@ -1,5 +1,14 @@
 package com.softwaremill.adopttapir.template.scala
 
+import com.softwaremill.adopttapir.starter.ScalaVersion
+
 case class Import(fullName: String) {
-  def asScalaImport(): String = s"import $fullName"
+  def asScalaImport(scalaVersion: ScalaVersion): String = {
+    val adjustedImport = scalaVersion match {
+      case ScalaVersion.Scala2 => fullName
+      case ScalaVersion.Scala3 => fullName.replace("._", ".*").replace(".{_", ".{*").replace("_}", "*}")
+    }
+
+    s"import $adjustedImport"
+  }
 }
