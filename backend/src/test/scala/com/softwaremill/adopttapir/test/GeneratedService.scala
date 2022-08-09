dd20b5aeb9d055c28b57f5ad6b7202c8811e1219
Jacek Centkowski <jcentkowski@softwaremill.com>
2022-08-09 14:38:06 +0200

    feat: verify scala-cli configurations in GitHub CI
    Extend the existing integration tests to cover also `scala-cli` builder
    configuratiion.
    
    Notes:
    * the existing `adopt-tapir-ci.yml` was extended with
      `VirtusLab/scala-cli-setup@v0.1` and 'coursier/cache-action@v6.3'
      actions
    * JVM setup was removed as JVM is being set by installation of coursier
      (for scala-cli)
    * 'scala-cli' cannot be chained to call compile, test and run in a
      signle line therefore compile and test is called when
      'ScalaCliService' is created and then run is spawned. It affects the
      resource creation therefore it is a subject of timeout (by default
      30s) therefore new 'waitForScalaCliCompileAndUnitTest' (45s) was
      introduced to add a bit more space to it so that CI will not time out
    * 'scala-cli' tends to fail on the first test start as it waits for
      server to finish (while sbt is being called on the other CI thread):
      ensure that only 1 IT test is called on a dedicated runner
    * 'waitForPort' failure was improved to contain full stack exception
      stack trace so that it is easier to debug what failed
    * StarterServiceITTest was refactored so that it contains less code:
      Servce (renaned to GeneratedService) landed in its own file; also
      improved describers (for both Throwable and StarterDetails) landed in
      a dedicated file;
    
    Part of #45

---
+++ backend/src/test/scala/com/softwaremill/adopttapir/test/GeneratedService.scala
@@ -0,0 +1,114 @@
+package com.softwaremill.adopttapir.test
+
+import cats.effect.IO
+import com.softwaremill.adopttapir.starter.Builder
+import org.scalatest.Assertions
+import os.SubProcess
+
+import java.time.LocalDateTime
+import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.concurrent.TimeoutException
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.util.matching.Regex
+
+object ServiceTimeouts {
+  val waitForScalaCliCompileAndUnitTest: FiniteDuration = 45.seconds
+  val waitForPortTimeout: FiniteDuration = 90.seconds
+}
+
+abstract class GeneratedService {
+  import ServiceTimeouts.waitForPortTimeout
+  import Describers.ThrowableWithDescribe
+
+  protected val portPattern: Regex
+  protected val process: SubProcess
+
+  val port: IO[Integer] = {
+    val stdOut = new mutable.StringBuilder()
+    IO.blocking {
+      val port = waitForPort(stdOut)
+      assert(port > -1)
+      port
+    }.timeoutAndForget(waitForPortTimeout)
+      .onError(e =>
+        Assertions.fail(
+          s"Detecting port of the running server failed ${if (e.isInstanceOf[TimeoutException]) s"due to timeout [${waitForPortTimeout}s]"
+            else s"Exception:${System.lineSeparator()}${e.describe()}"} with process std output:${System.lineSeparator()}$stdOut"
+        )
+      )
+  }
+
+  @tailrec
+  private def waitForPort(stdOut: mutable.StringBuilder): Integer = {
+    if (process.stdout.available() > 0 || process.isAlive()) {
+      val line = process.stdout.readLine()
+      if (line == null) {
+        -1
+      } else {
+        stdOut.append("### process log <").append(new Timestamper).append(line).append(">").append(System.lineSeparator())
+        portPattern.findFirstMatchIn(line) match {
+          case Some(port) => port.group(1).toInt
+          case None       => waitForPort(stdOut)
+        }
+      }
+    } else {
+      -1
+    }
+  }
+
+  def close(): IO[Unit] = IO.blocking {
+    if (process.isAlive()) process.close()
+  }
+
+  private class Timestamper {
+    private val timestamp = LocalDateTime.now()
+
+    override def toString: String = s"[$timestamp]"
+  }
+}
+
+object GeneratedService {
+  import ServiceTimeouts.waitForScalaCliCompileAndUnitTest
+
+  def apply(builder: Builder, tempDir: better.files.File): IO[GeneratedService] = {
+    builder match {
+      case Builder.Sbt      => IO.blocking(SbtService(tempDir))
+      case Builder.ScalaCli => IO.blocking(ScalaCliService(tempDir)).timeoutAndForget(waitForScalaCliCompileAndUnitTest)
+    }
+  }
+
+  private case class SbtService(tempDir: better.files.File) extends GeneratedService {
+    override protected val portPattern = new Regex("^(?:\\[info\\] )(?:Go to |Server started at )http://localhost:(\\d+).*")
+
+    override protected lazy val process: SubProcess = {
+      os.proc(
+        "sbt",
+        "-no-colors",
+        // start in forked mode so that process input can be forwarded and process waits before closing otherwise `StdIn.readLine` will exit immediately
+        "set run / fork := true",
+        // forward std input to forked process - https://www.scala-sbt.org/1.x/docs/Forking.html#Configuring+Input
+        "set run / connectInput := true",
+        ";compile ;test ;run"
+      ).spawn(cwd = os.Path(tempDir.toJava), env = Map("http.port" -> "0"), mergeErrIntoOut = true)
+    }
+  }
+
+  private case class ScalaCliService(tempDir: better.files.File) extends GeneratedService {
+    override protected val portPattern = new Regex("^(?:Go to |Server started at )http://localhost:(\\d+).*")
+
+    override protected lazy val process: SubProcess = {
+      // one cannot chain multiple targets to scala-cli hence 'test' target (that implicitly calls compile) is called in
+      // blocking manner and once it returns with success (0 exit code) the configuration is actually started
+      val compileAndTest = os.proc("scala-cli", "test", ".").call(cwd = os.Path(tempDir.toJava), mergeErrIntoOut = true)
+      assert(
+        compileAndTest.exitCode == 0,
+        s"Compilation and unit tests exited with [${compileAndTest.exitCode}] and output:${System
+            .lineSeparator()}${compileAndTest.out.lines().mkString(System.lineSeparator())}"
+      )
+
+      os.proc("scala-cli", "run", ".")
+        .spawn(cwd = os.Path(tempDir.toJava), env = Map("http.port" -> "0"), mergeErrIntoOut = true)
+    }
+  }
+}
