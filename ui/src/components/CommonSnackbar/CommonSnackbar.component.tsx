a14e8858ed4ad47278791c2d240631f5b05becfc
Jacek Centkowski <jcentkowski@softwaremill.com>
2022-09-12 17:25:06 +0200

    feat: introduce CommonSnackbar
    There are two places where snackbar is used therefore it makes sense to
    introduce CommonSnackbar so that their look & feel is aligned.
    Considering these two usage the CommonSnackbar has properties according
    to this interface:
    
    type Props = {
      onClose: () => void;
      open: boolean;
      duration?: number;
      message?: String;
      severity?: AlertColor;
    };
    
    Note that `ApiCallAddons` implementation contained `outlined` property
    additionally but it was gave up due to being less readable on smaller
    screens (overlapped with background).
    
    Result of #75

---
+++ ui/src/components/CommonSnackbar/CommonSnackbar.component.tsx
@@ -0,0 +1,26 @@
+import { Alert, AlertColor, Snackbar } from '@mui/material';
+
+type Props = {
+  onClose: () => void;
+  open: boolean;
+  duration?: number;
+  message?: String;
+  severity?: AlertColor;
+};
+
+export function CommonSnackbar({ onClose, open, severity, message, duration }: Props) {
+  return (
+    <>
+      <Snackbar
+        open={open}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        autoHideDuration={duration || 5000}
+        onClose={onClose}
+      >
+        <Alert onClose={onClose} severity={severity || 'error'} sx={{ width: '100%' }}>
+          {message}
+        </Alert>
+      </Snackbar>
+    </>
+  );
+}
