97351583e0195ea94c89e9eba055b5ed2ae9a34a
Mateusz Piękos <mateuszpiekos@gmail.com>
2025-12-18 19:06:34 +0100

    removed SetCorrelationIdBackend

--- backend/src/main/scala/com/softwaremill/adopttapir/infrastructure/CorrelationId.scala
+++ backend/src/main/scala/com/softwaremill/adopttapir/infrastructure/CorrelationId.scala
@@ -24,25 +24,6 @@ object CorrelationId:
     local <- IOLocal[Option[String]](None)
   yield CorrelationId(local, random)
 
-//** An sttp backend wrapper, which sets the current correlation id on all outgoing requests. */
-// Note: This is not currently used in the codebase. If needed, it should be updated to match sttp client4 API.
-// In client4, backend wrappers need to implement the correct backend trait (Backend, StreamBackend, etc.)
-// and handle GenericRequest types properly.
-// class SetCorrelationIdBackend(delegate: Backend[IO])(using correlationId: CorrelationId) extends Backend[IO]:
-//   import CorrelationIdInterceptor.*
-//
-//   override def send[T, P](request: GenericRequest[T, P]): IO[Response[T]] =
-//     correlationId.get
-//       .map {
-//         case Some(cid) => request.header(CorrelationIdInterceptor.HeaderName, cid)
-//         case None      => request
-//       }
-//       .flatMap(request => request.send(delegate))
-//
-//   override def close(): IO[Unit] = delegate.close()
-//
-//   override def monad: MonadError[IO] = delegate.monad
-
 /** A tapir interceptor, which reads the correlation id from the headers; if it's absent, generates a new one. */
 class CorrelationIdInterceptor private (using correlationId: CorrelationId) extends RequestInterceptor[IO]:
   import CorrelationIdInterceptor.*
